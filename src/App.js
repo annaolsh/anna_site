a0560ecaab9bc1cc15c21473c8f9543aa52e39b1
github username <olshevskaia.anna@gmail.com>
2018-09-28 17:56:15 -0400

    Added header, about me, skills components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from './components/Header.js';
+import MainContainer from './containers/MainContainer.js';
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faStar);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Header></Header>
+        <MainContainer></MainContainer>
       </div>
     );
   }
---
+++ src/components/Skills.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import skills from '../assets/json/skills.json';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default class Skills extends Component {
+    constructor() {
+        super()
+        this.state = {
+            skills: skills
+        }
+    }
+
+    _numberToStars(number) {
+        if (number === 5) {
+            return (
+                <span className="star">
+                    <FontAwesomeIcon icon="star" color="#6DB65B" size="sm"/>
+                    <FontAwesomeIcon icon="star" color="#6DB65B" size="sm"/>
+                    <FontAwesomeIcon icon="star" color="#6DB65B" size="sm"/>
+                    <FontAwesomeIcon icon="star" color="#6DB65B" size="sm"/>
+                    <FontAwesomeIcon icon="star" color="#6DB65B" size="sm"/>
+                </span>
+            )
+        } else if (number === 4) {
+            return (
+                <span className="star">
+                    <FontAwesomeIcon icon="star" color="#6DB65B" size="sm"/>
+                    <FontAwesomeIcon icon="star" color="#6DB65B" size="sm"/>
+                    <FontAwesomeIcon icon="star" color="#6DB65B" size="sm"/>
+                    <FontAwesomeIcon icon="star" color="#6DB65B" size="sm"/>
+                    <FontAwesomeIcon icon="star" color="#rrr" size="sm"/>
+                </span>
+            )
+        } else {
+            return (
+                <span className="star">
+                    <FontAwesomeIcon icon="star" color="#6DB65B" size="sm"/>
+                    <FontAwesomeIcon icon="star" color="#6DB65B" size="sm"/>
+                    <FontAwesomeIcon icon="star" color="#6DB65B" size="sm"/>
+                    <FontAwesomeIcon icon="star" color="#rrr" size="sm"/>
+                    <FontAwesomeIcon icon="star" color="#rrr" size="sm"/>
+                </span>
+            )
+        }
+    }
+
+    render() {
+        return (
+            <div id="skills-component">
+                <h2>Skills</h2>
+                <div id="skillset" className="row">
+                    <div className="col-md-6">
+                        <ul>
+                            {this.state.skills.slice(0, 6).map((skill, i) => {
+                                return (
+                                    <li>
+                                        <span className="skill-name">{skill.name}</span>
+                                        {this._numberToStars(skill.value)}
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                    <div className="col-md-6">
+                        <ul>
+                            {this.state.skills.slice(6).map((skill, i) => {
+                                return (
+                                    <li>
+                                        <span className="skill-name">{skill.name}</span>
+                                        {this._numberToStars(skill.value)}
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    
+}
